7cae5ba90a73c3c57f6bf790b349b87193a2004b
Gouravsen1202 <gouravsen757@gmail.com>
2024-11-25 13:50:40 +0530

    first commit

---
+++ Untitled2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	for(int r=1;r<=9;r++){
+	
+ 	(r<=5)?a++:a--;
+		for(int c=1;c<=9;c++){
+			if(c>=6-a&& c<=4+a){
+				cout<<"*";			}
+		
+			else{
+				cout<<" ";
+			}
+		}cout<<endl;
+	}
+}
